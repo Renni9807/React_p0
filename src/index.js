bd3d7870236a56fe73d719bf6085e8d4dc7453e4
Renni9807 <practiceID98@gmail.com>
2024-07-20 03:10:21 +0800

    Amazon Best seller

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import React from "react";
+import "./index.css";
+import book1 from "./img/book1.jpg";
+import book2 from "./img/book2.jpg";
+import book3 from "./img/book3.jpg";
 
-// function Greeting() {
-//   return <h2>My First Component in React App. </h2>;
-// }
-function Greeting() {
-  return React.createElement("h2", {}, "Hello World!");
-}
+const BookList = () => {
+  return (
+    <section>
+      <h1>Amazon Best Sellers</h1>
+      <ul>
+        <li>
+          <div className="book">
+            <img
+              src={book1}
+              alt="Interesting Facts For Curious Minds"
+              srcset=""
+            />
+            <p>Interesting Facts For Curious Minds</p>
+            <p>Jordan Moore</p>
+          </div>
+        </li>
+        <li>
+          <div className="book">
+            <img src={book2} alt="Atomic Habits" srcset="" />
+            <p>Atomic Habits</p>
+            <p>James Clear</p>
+          </div>
+        </li>
+        <li>
+          <div className="book">
+            <img src={book3} alt="Fairy Tale" srcset="" />
+            <p>Fairy Tale</p>
+            <p>Stephen King</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+};
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<Greeting />);
+root.render(<BookList />);
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <Person />
+//       <Message />
+//     </div>
+//   );
+// }
+
+// const Person = () => <h2>john doe</h2>;
+// const Message = () => {
+//   return <p>this is my message</p>;
+// };
+
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+// root.render(<Greeting />);
